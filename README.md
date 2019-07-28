6736a877ad499a8c544eec9352ea59b04bdd7ab8
Michael Dijkstra <micdijkstra@gmail.com>
2019-07-29 06:11:39 +1000

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Now Test
+
+Example site for testing now
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+const Index = () => (
+  <>
+    <div className="wrapper">
+      <div className="container">
+        <h1>Hello Next.js</h1>
+      </div>
+    </div>
+    <style jsx>{`
+      body {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+          Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+          'Segoe UI Symbol';
+      }
+
+      .wrapper {
+        align-items: center;
+        display: flex;
+        height: 100vh;
+        width: 100%;
+      }
+
+      .container {
+        margin: auto;
+      }
+    `}</style>
+  </>
+)
+
+export default Index
