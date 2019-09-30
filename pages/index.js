2d5bf7e6ff1ea10a279ddabd5d30356691b7569c
Michael Dijkstra <micdijkstra@gmail.com>
2019-09-30 12:46:21 +1000

    Bump

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Index = () => (
   <>
     <div className="wrapper">
       <div className="container">
-        <h1>Hello Next.js 3</h1>
+        <h1>Hello Next.js 3 bump</h1>
       </div>
     </div>
     <style jsx>{`
