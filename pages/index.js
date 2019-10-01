c74daa7fd200be165648c8fd6116e33b27a05cdf
Michael Dijkstra <micdijkstra@gmail.com>
2019-10-01 12:13:25 +1000

    Nexter

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Index = () => (
   <>
     <div className="wrapper">
       <div className="container">
-        <h1>Hello Next</h1>
+        <h1>Hello Nexter</h1>
       </div>
     </div>
     <style jsx>{`
