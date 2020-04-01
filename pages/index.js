b5b8fc5967826890d8540d02862e5c51098d19ea
Michael Dijkstra <micdijkstra@gmail.com>
2020-04-01 12:25:04 +1100

    Bump 2

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Index = () => (
   <>
     <div className="wrapper">
       <div className="container">
-        <h1>Hello World 13!</h1>
+        <h1>Hello World 13-2!</h1>
       </div>
     </div>
     <style jsx>{`
