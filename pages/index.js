e4ba3de1b6c3f7b94238d5e373b64c4a4dac1a53
Michael Dijkstra <micdijkstra@gmail.com>
2020-04-09 14:29:34 +1000

    Direct to master

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ const Index = () => (
   <>
     <div className="wrapper">
       <div className="container">
-        <h1>Hello World Master</h1>
+        <h1>Hello World Master 2</h1>
       </div>
     </div>
     <style jsx>{`
