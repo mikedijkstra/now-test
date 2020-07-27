c23c60c376c9cbda71132efcc6f4b7c7cc96501d
Michael Dijkstra <michael@dijkstra.io>
2020-07-28 09:05:19 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,8 @@ const Index = () => (
     <div className="wrapper">
       <div className="container">
         <h1>Hello World!</h1>
-        <img
-          src="https://www.istockphoto.com/resources/images/HomePage/Billboard/FreeImagesSupport-iStock-175245308.jpg"
-          alt="Stock Image"
-        />
       </div>
     </div>
-    <style jsx>{`
-      body {
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-          Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
-          'Segoe UI Symbol';
-      }
-
-      .wrapper {
-        align-items: center;
-        display: flex;
-        height: 100vh;
-        width: 100%;
-      }
-
-      .container {
-        margin: auto;
-      }
-    `}</style>
   </>
 )
 
